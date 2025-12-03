6ee1b0c277bb2bc27635c04692cb1cd02fafc1ab
myouo <chizurumyo@gmail.com>
2025-12-03 16:02:32 +0800

    no_repeats definitely passed!

--- hw08/hw08.scm
+++ hw08/hw08.scm
@@ -25,4 +25,31 @@
 
 )
 
-(define (no-repeats s) 'YOUR-CODE-HERE)
+(define (no-repeats s) 
+    (define (my-member val s)
+        (cond
+            [(null? s) #f]
+            [(= val (car s)) #t]
+            [else (my-member val (cdr s))]
+        )
+    )
+
+    (define (my-reverse s)
+        (define (helper s acc)
+            (if (null? s)
+                acc
+                (helper (cdr s) (cons (car s) acc))
+            )
+        )
+        (helper s '())
+    )
+
+    (define (helper s seen)
+        (cond
+            [(null? s) seen]
+            [(my-member (car s) seen) (helper (cdr s) seen)]
+            [else (helper (cdr s) (cons (car s) seen))]
+        )
+    )
+    (my-reverse (helper s '()))
+)
