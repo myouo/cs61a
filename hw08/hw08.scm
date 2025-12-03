2a868c318e54ea49eaa507e30e8553b0c8271bf6
myouo <chizurumyo@gmail.com>
2025-12-03 15:15:16 +0800

    interleave clear~

--- hw08/hw08.scm
+++ hw08/hw08.scm
@@ -16,6 +16,13 @@
      (my-filter pred (cdr s))]))
 
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (interleave lst1 lst2) 
+    (cond
+        [(null? lst1) lst2]
+        [(null? lst2) lst1]
+        [else (cons (car lst1) (cons (car lst2) (interleave (cdr lst1) (cdr lst2))))]
+        )
+
+)
 
 (define (no-repeats s) 'YOUR-CODE-HERE)
