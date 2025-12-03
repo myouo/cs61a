a3d28e87fdef88269bbc8714a9e284533d896f13
myouo <chizurumyo@gmail.com>
2025-12-04 00:31:15 +0800

    curry_cook and curry_consume done.

--- hw09/hw09.scm
+++ hw09/hw09.scm
@@ -1,7 +1,21 @@
-(define (curry-cook formals body) 'YOUR-CODE-HERE)
+(define (curry-cook formals body)
+  (if (null? formals)
+      body
+      (let ((first (car formals))
+            (rest  (cdr formals)))
+        (list 'lambda (list first)
+              (curry-cook rest body)))))
+
 
 (define (curry-consume curry args)
-  'YOUR-CODE-HERE)
+  (if (null? args)
+      curry
+      (curry-consume (curry (car args))
+                     (cdr args)
+      )
+  )
+)
+
 
 (define-macro (switch expr options)
   (switch-to-cond (list 'switch expr options)))
